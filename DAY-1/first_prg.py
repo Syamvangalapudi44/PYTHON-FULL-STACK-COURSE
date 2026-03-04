2867a3992db5baf48692d1752caf84c9252c2a13
Syamvangalapudi44 <163495319+Syamvangalapudi44@users.noreply.github.com>
2026-03-04 17:24:05 +0530

    Add files via upload
    DAY-1 PYTHON CODE IN CODEGNAN

---
+++ DAY-1/first_prg.py
@@ -0,0 +1,4 @@
+print("This is my fist code in day-1")
+print("I am tarun and i am from s.r.nagar ,Hyderabad")
+print("my registrion id = CGH2931")
+print("my skills are python,mysql,frontend,html")
