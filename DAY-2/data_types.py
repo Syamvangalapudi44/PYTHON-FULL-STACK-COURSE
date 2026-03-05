6d032364a6a0701f9463889bd8f7c1ebad835cf7
Syamvangalapudi44 <163495319+Syamvangalapudi44@users.noreply.github.com>
2026-03-05 22:12:37 +0530

    Add files via upload
    DAY-2 Data types in Python

---
+++ DAY-2/data_types.py
@@ -0,0 +1,44 @@
+student_id="CGH2931"
+batch_no="PFS-50"
+email_adddress="syamnaidu400@gmail.com"
+age=21
+name="V.Syam Tarun "
+gender="M"
+state="Telangana"
+location="Hyderabad"
+Student_mobile_number=7993996344
+Parent_mobile_number=9949873257
+Github_link="https://github.com/Syamvangalapudi44"
+college_name="CMR COLLEGE OF ENGINEERING AND TECHNOLOGY"
+usn_number="22H51A0463"
+Qualification="under graduation"
+Department="ECE"
+Pass_out_year=2026
+Gradution_percentage=83
+skills=("python","sql","flask","excel","power BI","Pandas")
+
+print("-------PERSONAL INFORMATION ---------")
+print(f"my student id is {student_id}")
+print(f"batch_no is {batch_no}")
+print(f"email_adddress = {email_adddress}")
+print(f"age is {age}")
+print(f"NAME-{name}")
+print(f"gender is {gender}")
+print(f"state is {state}")
+print(f"location={location}")
+print(f"MOBILE NUMBER {Student_mobile_number}")
+print(f"Parent_mobile_number{Parent_mobile_number}")
+print(f"Github_link-{Github_link}")
+
+
+print("---------ACADEMIC INFORMATION ---------" )
+print(college_name)
+print(usn_number)
+print(Qualification)
+print(Department)
+print(Pass_out_year)
+print(Gradution_percentage)
+print(f"I know these {skills}")
+
+
+
