362a0897b375f24022c415b5f0debd6eb48aad0b
Syamvangalapudi44 <163495319+Syamvangalapudi44@users.noreply.github.com>
2026-03-11 17:15:53 +0530

    Add files via upload
    DAY-4 AT CODEGNAN

---
+++ DAY-4/control_state.py
@@ -0,0 +1,49 @@
+#----------list------------
+
+"""
+products=["bread","butter","milk","jam"]
+for item in products:
+    print(item)
+
+"""
+#------------tuple--------
+"""
+products=("bread","butter","milk","jam")
+for item in products:
+    print(item)
+"""
+#---------set-----------
+"""
+products={"bread","butter","milk","jam"}
+for item in products:
+    print(item)
+
+"""
+
+#--------dict-------------
+"""
+products={'bread':30,'butter':45,'milk':35,'jam':20}
+for item in products:
+    print("product name ",item)
+    print("product price",products[item])
+    print("buy now:add to cart")
+    print("add to fav ")
+    print("-------------")
+
+"""
+
+#---------------string--------------
+"""
+a="python programming"
+for ch in a:
+    print(ch)
+"""
+
+#---------Range---------------
+"""
+RANGE FUNCTION = (START,STOP+1,STEP)==(0,STEP+1,1)
+
+"""
+
+for i in range(1,99,2):
+    print(i)  
